5d87abd4982497d36d8f4f201cae5e277a768919
atheer <atheer@pop-os.localdomain>
2020-10-24 18:38:39 +0300

    👁

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
+function fileChange() {
+  var file = document.getElementById("input_img");
+  var form = new FormData();
+  form.append("image", file.files[0]);
 
-function fileChange(){
-    var file = document.getElementById('input_img');
-    var form = new FormData();
-    form.append("image", file.files[0])
-    
-    var settings = {
-      "url": "https://api.imgbb.com/1/upload?key=PUT YOUR KEY HERE",
-      "method": "POST",
-      "timeout": 0,
-      "processData": false,
-      "mimeType": "multipart/form-data",
-      "contentType": false,
-      "data": form
-    };
-    
-    
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-      var jx = JSON.parse(response);
-      console.log(jx.data.url);
-    
-    
-    });
-    }
+  var settings = {
+    url: "https://api.imgbb.com/1/upload?key=7bcfad647ce27bcb52f097ea42f14275",
+    method: "POST",
+    timeout: 0,
+    processData: false,
+    mimeType: "multipart/form-data",
+    contentType: false,
+    data: form,
+  };
+
+  $.ajax(settings).done(function (response) {
+    var jx = JSON.parse(response);
+
+    console.log(jx.data.url);
+  });
+}
